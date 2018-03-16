df5342682d82504a60d1b510923b0ab10e784ffe
Red Fox <foxilator@live.com>
2018-03-16 13:53:55 -0400

    wcf dll

---
+++ WeatherService/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WeatherService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public ImmediateWeather GetData(string value)
+        {
+            WeatherService srv = new WeatherService();
+            return srv.GetWeather(srv.GetStream(value));
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.Equals(composite.Weather))
+            {
+                composite.Weather = new ImmediateWeather();
+            }
+            return composite;
+        }
+    }
+}
